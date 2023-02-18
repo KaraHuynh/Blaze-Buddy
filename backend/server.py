12c126ae22c0a854812058a50ae2b72d12697315
Maryam Elbeshbishy <53820275+maryam-elbeshbishy@users.noreply.github.com>
2023-02-18 17:40:49 -0500

    Setting up boilerplate for Flask & React
    Backend located in folder - "Backend"
    
    Frontend located in folder - "blaze-buddy"

---
+++ backend/server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+# members api route
+@app.route("/")
+def welcome():
+    return {"welcome": ["hello!"]}
+
+# members api route
+@app.route("/members")
+def members():
+    return {"members": ["Member1","Member2","Member3"]}
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
