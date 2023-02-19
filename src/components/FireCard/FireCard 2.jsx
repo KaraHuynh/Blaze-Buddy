4b3e55e78d02770a63d5c9e7c71b6e95116abac7
Kara Huynh <karahuynh122@gmail.com>
2023-02-19 05:31:10 -0500

    dont know

--- src/components/FireCard/FireCard 2.jsx
+++ src/components/FireCard/FireCard 2.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 import { Card } from '@mui/material';
-import CardContent from '@mui/material/CardContent';
+import { CardContent } from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
 import './style.css';
 
 export default function FireCard ({ title, date, time, location, description }){
     return (
-        <Card className="card">
+        <Card>
             <CardHeader
-            
                 title={title}
                 subheader={location}
             />
             <CardContent>
-                <p>
-                      {description}
-                </p>
-                <p>
-                  
+                <h2>
                     {date}<br></br>
                     {time}
+                </h2>
+                <p>
+                    {description}
                 </p>
             </CardContent>
         </Card>
