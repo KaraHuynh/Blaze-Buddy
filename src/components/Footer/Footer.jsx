00d7135c4a37aab9f53a70e1b9f866dcb824feb5
Kara Huynh <karahuynh122@gmail.com>
2023-03-29 11:35:29 -0400

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './style.css';
 export default function Footer () {
     return(
         <footer className="footer">
-            <h1>© snaccs 2022</h1>
+            <h1>© snaccs 2023</h1>
         </footer>
     );
 }
